ec2460836255826bebc73d45fecba9972f97de4f
Craig Wermert <craig@wermert.me>
2016-12-19 15:50:25 -0500

    Front end

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -1,10 +1,20 @@
- // initialize swagger client, point to a resource listing
-  window.client = new SwaggerClient({
-    url: "http://petstore.swagger.io/v2/swagger.json",
-    success: function() {
-      // upon connect, fetch a pet and set contents to element "mydata"
-      client.pet.getPetById({petId:1},{responseContentType: 'application/json'}, function(data) {
-        document.getElementById("api").innerHTML = JSON.stringify(data.obj);
-      });
-    }
-  });
+var messageInput = document.querySelector('.message');
+
+function postMessage (msg) {
+  fetch('/api/messages', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      message: msg
+    })
+  })
+}
+
+function keyWasPressed(event) {
+  if (event.key === "Enter") {
+    postMessage(messageInput.value);
+    // Post 
+  }
+}
