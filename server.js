f2f333e20d1a4ebf14f05634b11be72cefe700ae
Craig Wermert <craig@wermert.me>
2016-12-19 13:44:22 -0500

    Proper mLab credentials. For reals.

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 var port     = process.env.PORT || 8080; // set our port
 
 var mongoose   = require('mongoose');
-mongoose.connect('mongodb://cwermert:OmE4vZA1Y!Je@ds139448.mlab.com:39448/heroku_wtlzvrbs'); // connect to our database
+mongoose.connect(process.env.MONGODB_URI); // connect to our database
 var Message     = require('./app/models/message');
 
 let splitIt = function (str) {
